7ebe3aced2f344652a41f939277e82518f5189e8
Fadilulahi Opeyemi Musediq <musediqopeyemi@gmail.com>
2026-06-22 14:42:53 -0400

    corrections updated

--- frontend/src/utils/exportUtils.js
+++ frontend/src/utils/exportUtils.js
@@ -1,8 +1,19 @@
 import * as XLSX from 'xlsx';
 
 export const exportToExcel = (data, fileName = 'transactions') => {
-    if(!data || data.length === 0) {
+    /* if(!data || data.length === 0) {
+        alert("No data to export!");
+        return;
+    } */
+
+    if (!Array.isArray(data) || data.length === 0) {
         alert("No data to export!");
         return;
     }
 }
+
+/*  const worksheet = XLSX.utils.json_to_sheet(data);
+  const workbook = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(workbook, worksheet, "Transactions");
+  XLSX.writeFile(workbook, `${fileName}.xlsx`);
+; */
